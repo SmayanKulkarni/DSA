c09a9b02fc15eae32b570c24ba1b27a0d1ed529d
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-03-18 20:47:42 +0530

    .

--- Basics of OOPS/Getters and Setters/mainstudent2.cpp
+++ Basics of OOPS/Getters and Setters/mainstudent2.cpp
@@ -7,8 +7,8 @@ int main()
 {
     Student s1;
     Student *s2 = new Student;
-    s1.setAge(34, 123);
-    s2->setAge(32,345);
+        s1.setAge(34, 123);
+        s2->setAge(32,345);
     s1.display();
     s2->display();
     return 0;
