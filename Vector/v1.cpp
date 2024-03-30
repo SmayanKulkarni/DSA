34a4a2ac965cfb2fb9a32b4ce8124a9247183cc8
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-03-30 22:37:50 +0530

    .

---
+++ Vector/v1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(12);
+    v.push_back(43);
+    v.push_back(34);
+    v.push_back(34);
+    cout<<v[0]<<endl;
+    cout<<v[1]<<endl;
+    cout<<v[2]<<endl;
+    cout<<v[3]<<endl;
+    cout<<v[4]<<endl;
+    
+    return 0;
+
+}
