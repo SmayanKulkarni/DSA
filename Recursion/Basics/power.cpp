b6e30793058a196fcbddddae68fd3ae78af23052
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-04-01 19:23:58 +0530

    recursion

---
+++ Recursion/Basics/power.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pow(int d, int n)
+{
+    //base case
+    if(n==0) return 1;
+    if(n==1) return d;
+    // recursive case 
+    int smallans = pow(d,n-1);
+    //final ans
+    return d*smallans;
+
+
+}
+
+int main()
+{
+    int n,d;
+    cin>>d;
+    cin>>n;
+    cout<<pow(d, n)<<endl;
+    return 0;
+
+}
