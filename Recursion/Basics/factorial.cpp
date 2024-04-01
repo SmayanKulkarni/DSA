6dd2a2e40a07b234911700f09a10a0432eb94c14
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-04-01 18:34:01 +0530

    .

--- Recursion/Basics/factorial.cpp
+++ Recursion/Basics/factorial.cpp
@@ -4,8 +4,11 @@ using namespace std;
 int fact(int n)
 {
     if (n<0) return -1;
-    if(n ==0) return 1;
-     return n*fact(n-1);
+    if(n ==0) return 1;  // 1st step of PMI
+    int smallans = fact(n-1); // 2nd step of PMI, Recursive case
+    int ans = n*smallans; //3rd step of PMI : calculation
+    return ans;
+   
 }
 
 int main()
