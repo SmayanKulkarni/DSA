bc3b8c1cb52b920ecae6ed2d07a721173ce77ba1
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-04-06 15:55:25 +0530

    .

--- Recursion/Intermediate/firstandlastindex.cpp
+++ Recursion/Intermediate/firstandlastindex.cpp
@@ -18,6 +18,14 @@ int lastindex(int a[], int n,int x, int i)
 
     return lastindex(a,n,x,i-1);
 }
+int lastindex2(int a[], int n,int x)
+{
+    //base case
+    if(n==0) return -1;
+    if(a[n-1] == x)  return n-1;
+
+    return lastindex2(a,n-1,x);
+}
 
 int main()
 {
@@ -29,7 +37,8 @@ int main()
         cin>>a[i];
     }
     cin>>x;
-    cout<< lastindex(a,n,x,n)<<endl;
+    cout<< lastindex(a,n,x,n-1)<<endl;
+    cout<< lastindex2(a,n,x)<<endl;
    
     
     return 0;
