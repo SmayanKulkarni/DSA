5809894c9bb397725aa3f58ad5f5f1cc6e7cc765
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-04-26 19:58:17 +0530

    .

---
+++ Recursion/Advanced/lowercasetouppercase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void revstring(string &s, int st, int e)
+{
+    if(st>=e) 
+    {
+        
+        return;
+    }
+    swap(s[st],s[e]);
+    revstring(s,st+1, e-1);
+    
+}
+
+int main()
+{
+    string a= "aeiioodsd";
+    int len = a.length();
+    revstring(a,0,len-1);
+    cout<<a<<endl;
+    return 0;
+}
