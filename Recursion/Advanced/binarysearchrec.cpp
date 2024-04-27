61931ab3f73e2c2c4404da98b2bf0c673324b153
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-04-27 17:24:55 +0530

    .

---
+++ Recursion/Advanced/binarysearchrec.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std; 
+
+bool binary(int arr[],int x, int s, int e)
+{
+    if(s>e) return 0;
+    int mid = s+(e-s)/2;
+    if(arr[mid] ==x) {
+        return 1;
+    }
+    else if(arr[mid] >x){
+        return binary(arr,x,s,mid-1);
+    }
+    else {
+        return binary(arr,x,mid+1,e);;
+    }
+}
+
+int main()
+{
+       int arr[5];
+    for(int i=0;i<5;i++)
+    {
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    int a = binary(arr, x,0,5);
+    cout<<a<<endl;
+}
