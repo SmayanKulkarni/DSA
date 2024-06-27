67acdefebcec864a071be4fb2f26b3cca6810481
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-06-27 14:45:10 +0530

    .

--- Stacks/validparatheseslc20.cpp
+++ Stacks/validparatheseslc20.cpp
@@ -2,41 +2,33 @@
 #include <stack>
 using namespace std;
 
-bool isValid(string A)
-{
+bool isValid(string A){
     stack<char> s;
-    for (int i = 0; i < A.size(); i++)
-    {
-        if (A[i] == '(' || A[i] == '{' || A[i] == '[')
-        {
+    for (int i = 0; i < A.size(); i++){
+        if (A[i] == '(' || A[i] == '{' || A[i] == '['){
             s.push(A[i]);
         }
-        else if (s.empty())
-        {
+        else if (s.empty()){
             return false;
         }
-        else if (A[i] == ')')
-        {
+        else if (A[i] == ')'){
             if (s.top() == '(')
                 s.pop();
             else
                 return false;
         }
-        else if (A[i] == '}')
-        {
+        else if (A[i] == '}') {
             if (s.top() == '{')
                 s.pop();
             else
                 return false;
         }
-        else if (A[i] == ']')
-        {
+        else if (A[i] == ']'){
             if (s.top() == '[')
                 s.pop();
             else
                 return false;
-        }
-    }
+        }}
     if (s.empty())
         return true;
     return false;
