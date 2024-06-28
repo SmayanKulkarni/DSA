255ed0bb5afc644c227eac0c9cfd6aa58a8ffa0e
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-06-28 17:44:55 +0530

    .

--- Queues/queueLL.cpp
+++ Queues/queueLL.cpp
@@ -14,6 +14,7 @@ public:
         next = NULL;
     }
 };
+
 template <typename T>
 class Queue
 {
@@ -30,19 +31,42 @@ public:
     }
     int getsize()
     {
+        return size;
     }
     bool isEmpty()
     {
+        return size==0;
     }
     void push(T ele)
     {
+        Node<T>* n =  new Node<T>(ele);
+        if(isEmpty() || head==NULL) 
+        {
+            head = tail = n;
+        }
+        
+        tail->next = n;
+        tail = n;
+        size++;
     }
     T front()
     {
-
+        if(isEmpty())
+        {
+            return 0;
+        }
+        return head->data;
     }
     void pop()
     {
-        
+        if(isEmpty())
+        {
+            return;
+        }
+        Node<T>* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+        size--;
     }
 }
