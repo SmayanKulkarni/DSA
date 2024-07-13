ad2e7b5da3cf81cb681d0adaea0070aeec608a29
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-07-13 12:20:16 +0530

    .

--- Trees/Binary Trees/diameter-tree.cpp
+++ Trees/Binary Trees/diameter-tree.cpp
@@ -16,3 +16,40 @@ public:
         return max(opt1, max(opt2, opt3));
     }
 };
+
+
+
+//better approach 
+
+class Solution {
+public:
+
+    pair<int,int> hd(TreeNode* root)
+    {
+        if(!root){
+            pair<int,int> p;
+            p.first = 0;
+            p.second = 0;
+            return p;
+        }
+        
+        pair<int, int> leftans = hd(root->left);
+        pair<int, int> rightans = hd(root->right);
+
+        int lh = leftans.first;
+        int ld = leftans.second;
+        int rh = rightans.first;
+        int rd = rightans.second;
+
+        int height = 1 + max(lh, rh);
+        int diameter = max(lh + rh, max(ld,rd));
+        pair<int,int> p ;
+            p.first= height;
+            p.second = diameter;
+            return p;
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        pair<int, int> p = hd(root);
+        return p.second;
+    }
+};
