a959bbff1add97bcdf2fae9421d728f20d8d7ac0
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-07-19 19:56:07 +0530

    .

--- HashMaps/inbuilt-unordered.cpp
+++ HashMaps/inbuilt-unordered.cpp
@@ -9,4 +9,20 @@ int main()
     pair<string, int> p("abc", 1);
     my_map.insert(p);
     my_map["def"] = 2; // like array
+    cout << my_map["def"] << endl;
+    cout << my_map.at("abc") << endl;
+
+    // check presence of a key
+    if (my_map.count("ghi") > 0)
+        cout << "Key is present" << endl;
+    else
+        cout << "not Present" < endl;
+
+    // updating value
+    my_map["def"] = 3;
+    cout << my_map["def"] << endl;
+
+    // size
+
+    cout << my_map.size() << endl;
 }
