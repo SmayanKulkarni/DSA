2b1f4771bb2e4029c4ac92456ca1427007378ed7
SmayanKulkarni <smayankulkarni05@gmail.com>
2024-07-22 17:03:41 +0530

    .

--- HashMaps/separate-chaining.cpp
+++ HashMaps/separate-chaining.cpp
@@ -25,6 +25,19 @@ using namespace std;
         MapNode<V>** buckets;
         int count;
         int numbuckets;
+
+        int getbucketindex(string key){  //HashFunction
+            int hashcode = 0;
+            int base = 1;
+            int p=11;
+            for(i = key.size()-1; i>=0; i--){
+                hashcode += key[i] * base;
+                base = base*p;
+                hashcode = hashcode % numbuckets;
+                base = base % numbuckets;
+            }
+            return hashcode%numbuckets;
+        }
     public:
         mymap(){
             count = 0;
@@ -38,6 +51,20 @@ using namespace std;
                 delete buckets[i];
             }
             delete[] buckets;
+        }
+        int size()
+        {
+            return count;
+        }
+        
+        V getValue(string key){
+
+        }
+        void insert(string key, V value){
+
+        }
+        void remove(string key){
+
         }
 
   }
