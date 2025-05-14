4eccd00d29bd574c265642cb891335216d7dd694
Smayan Kulkarni <smayankulkarni05@gmail.com>
2025-05-14 20:26:11 +0530

    FIXES

--- Final Lab/2-BubbleSort.c
+++ Final Lab/2-BubbleSort.c
@@ -19,7 +19,7 @@ void BubbleSort(int arr[], int n){
         for (j=0; j<n-i-1;j++){
             if (arr[j] > arr[j+1])
             {
-                temp = arr[i];
+                temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
